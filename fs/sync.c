b748998add3dab4c991fdd70723c6a4358441ca9
CPA-Poke <brock.shahan@cox.net>
2014-04-17 01:04:04 -0500

    Add fsync toggle

--- fs/sync.c
+++ fs/sync.c
@@ -16,6 +16,11 @@
 
 #include <trace/events/mmcio.h>
 
+#include <linux/moduleparam.h>
+
+bool fsync_enabled = true;
+module_param(fsync_enabled, bool, 0755);
+
 #define VALID_FLAGS (SYNC_FILE_RANGE_WAIT_BEFORE|SYNC_FILE_RANGE_WRITE| \
 			SYNC_FILE_RANGE_WAIT_AFTER)
 
@@ -101,6 +106,9 @@ SYSCALL_DEFINE1(syncfs, int, fd)
 	int ret;
 	int fput_needed;
 
+	if (!fsync_enabled)
+			return 0;
+
 	file = fget_light(fd, &fput_needed);
 	if (!file)
 		return -EBADF;
@@ -128,6 +136,8 @@ SYSCALL_DEFINE1(syncfs, int, fd)
 int vfs_fsync_range(struct file *file, loff_t start, loff_t end, int datasync)
 {
 	int err;
+	if (!fsync_enabled)
+			return 0;
 	if (!file->f_op || !file->f_op->fsync)
 		return -EINVAL;
 	trace_vfs_fsync(file);
@@ -147,6 +157,8 @@ EXPORT_SYMBOL(vfs_fsync_range);
  */
 int vfs_fsync(struct file *file, int datasync)
 {
+	if (!fsync_enabled)
+			return 0;
 	return vfs_fsync_range(file, 0, LLONG_MAX, datasync);
 }
 EXPORT_SYMBOL(vfs_fsync);
@@ -157,6 +169,9 @@ static int do_fsync(unsigned int fd, int datasync)
 	int ret = -EBADF;
 	ktime_t fsync_t, fsync_diff;
 	char pathname[256], *path;
+
+	if (!fsync_enabled)
+			return 0;
 
 	file = fget(fd);
 	if (file) {
@@ -180,16 +195,22 @@ static int do_fsync(unsigned int fd, int datasync)
 
 SYSCALL_DEFINE1(fsync, unsigned int, fd)
 {
+	if (!fsync_enabled)
+			return 0;
 	return do_fsync(fd, 0);
 }
 
 SYSCALL_DEFINE1(fdatasync, unsigned int, fd)
 {
+	if (!fsync_enabled)
+			return 0;
 	return do_fsync(fd, 1);
 }
 
 int generic_write_sync(struct file *file, loff_t pos, loff_t count)
 {
+	if (!fsync_enabled)
+			return 0;
 	if (!(file->f_flags & O_DSYNC) && !IS_SYNC(file->f_mapping->host))
 		return 0;
 	return vfs_fsync_range(file, pos, pos + count - 1,
@@ -206,6 +227,9 @@ SYSCALL_DEFINE(sync_file_range)(int fd, loff_t offset, loff_t nbytes,
 	loff_t endbyte;			
 	int fput_needed;
 	umode_t i_mode;
+
+	if (!fsync_enabled)
+			return 0;
 
 	ret = -EINVAL;
 	if (flags & ~VALID_FLAGS)
