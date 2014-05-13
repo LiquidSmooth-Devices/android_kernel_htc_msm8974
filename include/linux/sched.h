7537bb3bdeb39137ade5c8159877d5f1f1b5fec4
Jake Weinstein <xboxlover360@gmail.com>
2014-05-13 15:28:25 -0500

    sched: added time-based average load
    From TripNRaver and maxwen
    Change-Id: Ia72f7014a86bc0fc4c745a3ce55b3dc5bae66f44

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -115,11 +115,14 @@ extern int nr_processes(void);
 extern unsigned long nr_running(void);
 extern unsigned long nr_uninterruptible(void);
 extern unsigned long nr_iowait(void);
+extern unsigned long avg_nr_running(void);
 extern unsigned long nr_iowait_cpu(int cpu);
 extern unsigned long this_cpu_load(void);
 
 extern void sched_update_nr_prod(int cpu, unsigned long nr, bool inc);
 extern void sched_get_nr_running_avg(int *avg, int *iowait_avg);
+extern unsigned long get_avg_nr_running(unsigned int cpu);
+extern unsigned long avg_nr_running(void);
 
 extern void calc_global_load(unsigned long ticks);
 
