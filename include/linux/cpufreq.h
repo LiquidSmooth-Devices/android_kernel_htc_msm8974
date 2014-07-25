1351d360601d84433495e4c524508773e3aa1c27
Jake Weinstein <xboxlover360@gmail.com>
2014-07-25 18:40:40 -0500

    Add SmartMAX from maxwen's Oppo 8974 kernel
    Change-Id: Iad1987f39cf165a250fae39caf203d7aa7cdcd0e

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -319,6 +319,9 @@ extern struct cpufreq_governor cpufreq_gov_intelliactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DANCEDANCE)
 extern struct cpufreq_governor cpufreq_gov_dancedance;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_dancedance)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTMAX)
+extern struct cpufreq_governor cpufreq_gov_smartmax;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_smartmax)
 #endif
 
 
