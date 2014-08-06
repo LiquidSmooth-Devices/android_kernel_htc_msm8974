b33f934c94e79a591b80f54ac9da7bcbd6a4425a
CPA-Poke <brock.shahan@cox.net>
2014-08-06 12:39:01 -0500

    intellidemand: re-introduce intellidemand 5.5 cpu governor
    Change-Id: I6b33e4e49dc5758206e0d89d48b79fdd35f649b8

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -322,6 +322,9 @@ extern struct cpufreq_governor cpufreq_gov_dancedance;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTMAX)
 extern struct cpufreq_governor cpufreq_gov_smartmax;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_smartmax)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIDEMAND)
+extern struct cpufreq_governor cpufreq_gov_intellidemand;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_intellidemand)
 #endif
 
 
